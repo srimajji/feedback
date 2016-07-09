1215c08add0cda790ea64edbf7e101dca4d0149a
srimajji <sri.majji@outlook.com>
2016-07-09 14:50:46 -0700

    remove unnecessary

--- client/src/reducers/FeedbackReducer.js
+++ client/src/reducers/FeedbackReducer.js
@@ -5,22 +5,7 @@ const initialState = [{
 }];
 
 function FeedbackReducer(state = initialState, action) {
-  switch (action.type) {
-  case "ADD_TODO":
-    return [{
-      id: (state.length === 0) ? 0 : state[0].id + 1,
-      marked: false,
-      text: action.text
-    }, ...state];
-
-  case "DELETE_TODO":
-    return state.filter(todo =>
-      todo.id !== action.id
-    );
-  
-  default:
-    return state;
-  }
+  return state;
 }
 
 export default FeedbackReducer;
