3bba341a12271f7702c03df6d8a315d552c5e5b5
Sri Majji <sri.majji@outlook.com>
2016-07-25 23:55:40 -0700

    use stateless components

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,23 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-class NavBar extends Component {
-    render() {
-        console.log(this.props.location);
-        return (
-            <nav>
-                <div className="nav-wrapper">
-                    <a href="#" className="brand-logo right">Logo</a>
-                    <ul id="nav-mobile" className="left hide-on-med-and-down">
-                        <li><Link to="admin">Home</Link></li>
-                        <li><Link to="admin/feedback">Feedback</Link></li>
-                        <li><Link to="admin/companies">Companies</Link></li>
-                        <li><Link to="admin/responses">Responses</Link></li>
-                    </ul>
-                </div>
-            </nav>
-        );
-    }
+const Navbar = () => {
+    return <div>This is a navbar </div>;
 }
 
-export default NavBar;
+export default Navbar;
--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 class Home extends React.Component {
     render() {
         return (
             <div>
+                <Navbar />
                 {this.props.children}
             </div>
         );
