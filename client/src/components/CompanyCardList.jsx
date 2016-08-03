b8b9fb09a489256a6bde9bfa003ea7cef2e4cf88
Sri Majji <sri.majji@outlook.com>
2016-08-03 00:23:48 -0700

    add ajax call in CompanyCardList

--- client/src/components/CompanyCardList.jsx
+++ client/src/components/CompanyCardList.jsx
@@ -3,14 +3,26 @@ import React, { Component } from 'react';
 import CompanyCard from './CompanyCard';
 
 class CompanyCardList extends Component {
-    render() {
-        const arr = [1, 2, 3, 4, 5];
-        const company = { name: 'Target', description: 'A store where you can get everything home related' };
+    constructor() {
+        super();
 
+        this.state = {
+            companies: []
+        };
+    }
+
+    componentDidMount() {
+        let companies = [];
+        $.get('http://localhost:3000/api/companies', function(data) {
+                this.setState({ companies: data });
+        }.bind(this));
+    }
+
+    render() {
         return (
             <div className='row'>
                 <ul className='col s12 m6'>
-                    {arr.map((num, index) => {
+                    {this.state.companies.map((company, index) => {
                         return <CompanyCard key={index} company={company} />;
                     })}
                 </ul>
