aaedbe9973a7e7615126b801c2a0ad771a6bf3ae
srimajji <sri.majji@outlook.com>
2016-08-17 04:46:23 -0700

    connect FeedbackReducer to FeedbackContainer

--- client/src/containers/FeedbackContainer.js
+++ client/src/containers/FeedbackContainer.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 
+import Card from '../components/Card';
+
+@connect(state => ({ ...state.FeedbackReducer }))
 
 class FeedbackContainer extends Component {
+    _newFeedback() {
+        return { type: 'FEEDBACK_NEW', title: 'New feedback', body: 'some body here' };
+    }
+
     render() {
-            console.log(this.context.store);
         const { feedbacks, dispatch } = this.props;
         return (
-            <div>
-                Feedback container
+            <div className='row'>
+                <div className='col s12'>
+                    { feedbacks.map(( feedback, key ) => {
+                        return <Card title={feedback.title} body={feedback.body} key={key}/>
+                    })}
+                    <a className='waves-effect waves-light btn' onClick={() => dispatch(this._newFeedback())}>New feedback</a>
+                </div>
             </div>
         );
     }
