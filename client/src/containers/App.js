157d17efaf8d74f4cb5bf1072ff7dca4b594c8d2
srimajji <sri.majji@outlook.com>
2016-08-23 02:57:18 -0700

    fix client prod settings

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -8,13 +8,14 @@ import routes from '../routes.js'
 
 class App extends Component {
     render() {
+        const env = process.env.NODE_ENV;
         const { store } = this.props;
         const history = syncHistoryWithStore(browserHistory, store);
         return (
             <Provider store={store}>
                 <div>
                     <Router history={history} routes={routes} />
-                    <DevTools />
+                    { env === 'development' ? <DevTools /> : null }
                 </div>
             </Provider>
         );
