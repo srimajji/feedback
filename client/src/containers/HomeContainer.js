9fa6141bd3d463d66097d5bc4e8c778cc79ca62a
Sri Majji <sri.majji@outlook.com>
2016-08-23 17:41:56 -0700

    fix feedback and company count to match actual objects within system in home container

--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import Navbar from '../components/Navbar';
 import Card from '../components/Card';
 
+@connect(state => ({ feedbacks: state.FeedbackReducer.feedbacks, companies: state.CompanyReducer.companies}))
 class HomeContainer extends Component {
     render() {
+        const { feedbacks, companies } = this.props;
         return (
             <div className='row'>
                 <div className='col s12'>
-                    <Card title={'Feedbacks'} body={'3'}/>
-                    <Card title={'Companies'} body={'3'}/>
-                    <Card title={'Users'} body={'3'}/>
+                    <Card title={'Feedbacks'} body={feedbacks.length}/>
+                    <Card title={'Companies'} body={companies.length}/>
                </div>
             </div>
         );
