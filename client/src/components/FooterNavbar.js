e45d9d945fea7ad732fab7fedb75cf0ce6d38cbf
Sri Majji <sri.majji@outlook.com>
2016-09-01 21:55:53 -0700

    refactor .jsx to .js

---
+++ client/src/components/FooterNavbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const FooterNavbar = () => {
+	return (
+		<footer className='page-footer'>
+			<Navbar />
+		</footer>
+	);
+}
+
+export default FooterNavbar;
